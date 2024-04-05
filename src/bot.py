f0b4cbfb1237b7c380482d6b330207abfbd9596b
zeraye <zeraye@protonmail.com>
2024-04-05 11:26:36 +0200

    feat: add random moves to bot

--- src/bot.py
+++ src/bot.py
@@ -2,7 +2,8 @@ from enums import BoardFields
 
 import numpy as np
 import numpy.typing as npt
+from random import randint
 
 
 def get_bot_move(board: npt.NDArray[np.int8], turn: BoardFields) -> int:
-    return 0
+    return randint(0, 6)
