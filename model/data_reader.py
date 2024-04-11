4211594441349a4dd6492785204daf867e76b855
Mateusz Tkaczyk <mateusztk@onet.eu>
2024-04-11 14:49:14 +0200

    fix: data reader uses board's enum

--- model/data_reader.py
+++ model/data_reader.py
@@ -1,5 +1,12 @@
 import numpy as np
 import os
+from enum import Enum
+
+
+class BoardFields(Enum):
+    EMPTY = 0
+    PLAYER1 = 1
+    PLAYER2 = 2
 
 
 NO_MOVE = -1000
@@ -14,20 +21,22 @@ def read_line(string):
     moves_string = values[0]
     evaluations = [int(values[i]) for i in range(1, 8)]
     indexes = [0 for _ in range(7)]
-    matrix = np.zeros((7, 6), dtype="int")
-    turn = 1
+    matrix = np.full((7, 6), BoardFields.EMPTY.value, dtype="int8")
+    turn = BoardFields.PLAYER1
 
     for move_string in moves_string:
         move = int(move_string) - 1
-        matrix[move, indexes[move]] = turn
+        matrix[move, indexes[move]] = turn.value
         indexes[move] += 1
-        turn *= -1
+        turn = (
+            BoardFields.PLAYER1 if turn == BoardFields.PLAYER2 else BoardFields.PLAYER2
+        )
 
     for i in range(len(evaluations)):
         if evaluations[i] == NO_MOVE:
             continue
         child_matrix = np.copy(matrix)
-        child_matrix[i, indexes[i]] = turn
+        child_matrix[i, indexes[i]] = turn.value
         x.append(child_matrix)
         y.append(evaluations[i])
 
@@ -43,6 +52,8 @@ for filename in os.listdir(cwd + "/data"):
         print("File not found.")
     except IOError:
         print("Error reading the file.")
+
+print(x[0])
 
 np.save(cwd + "/x.npy", x)
 np.save(cwd + "/y.npy", y)
