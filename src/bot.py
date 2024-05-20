f2fbf61e9cc03bbbda97ae692f1f6f63347a3fac
Mateusz Tkaczyk <mateusztk@onet.eu>
2024-05-20 16:53:00 +0200

    feat: add new models

--- src/bot.py
+++ src/bot.py
@@ -33,7 +33,9 @@ class Bot:
         return moves
     
     def get_bot_move(self, board: npt.NDArray[np.int8], turn: BoardFields) -> int:
-        if (self.model.layers[0].name == "regression with lines"):
+        if (self.model.layers[0].name == "regression: board and lines"):
+            return self.get_bot_move_regression2(board, turn)
+        elif (self.model.layers[0].name == "regression: lines"):
             return self.get_bot_move_regression2(board, turn)
         else:
             return self.get_bot_move_regression(board, turn)
@@ -41,21 +43,32 @@ class Bot:
 
     def get_bot_move_regression2(self, board: npt.NDArray[np.int8], turn: BoardFields) -> int:
         best_move = -1
-        best_eval = -np.Infinity
+        best_eval = np.Infinity
         predicts = [-1000.0 for _ in range(7)]
 
         for col in self.legal_moves(board):
             changed_board = board.copy()
-            changed_board[col, BOARD_ROWS - 1] = turn.value
+            board_column = changed_board[col]
+            row = (np.where(board_column == BoardFields.EMPTY.value)[0])[0]
+            changed_board[col, row] = turn.value
+            converted_board = convert_board(changed_board.copy())
 
-            converted_board = convert_board(board.copy())
             input = [np.array([converted_board[0]]), np.array([converted_board[1]]), np.array([converted_board[2]])]
+            if (self.model.layers[0].name == "regression: lines"):
+                input = [np.array([converted_board[1]]), np.array([converted_board[2]])]
+            
             predict = self.model.predict(input)
+
+            # TODO: Remove
+            #predict = converted_board[1][1] + 10 * converted_board[1][2] - converted_board[2][1] - 10 * converted_board[2][2]
+
             predicts[col] = float(predict)
 
-            if float(predict) > best_eval:
+            if float(predict) < best_eval:
                 best_eval = float(predict)
                 best_move = col
+
+        print(predicts)
 
         return best_move
 
